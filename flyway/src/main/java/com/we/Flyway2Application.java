aa4225c8f2c2ce56ec5c2da2ed369fd812b85f30
YilinSun <16216077@qq.comn>
2019-08-05 10:15:44 +0800

    Signed-off-by: YilinSun <16216077@qq.comn>

---
+++ flyway/src/main/java/com/we/Flyway2Application.java
@@ -0,0 +1,13 @@
+package com.we;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Flyway2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Flyway2Application.class, args);
+	}
+
+}
